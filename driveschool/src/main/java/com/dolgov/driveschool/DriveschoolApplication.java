d29afdf5617b160f1e74a3431566bc690089ba10
Alekseev_A <solmir505@gmail.com>
2025-02-22 04:07:10 +0800

    init

---
+++ driveschool/src/main/java/com/dolgov/driveschool/DriveschoolApplication.java
@@ -0,0 +1,13 @@
+package com.dolgov.driveschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveschoolApplication.class, args);
+	}
+
+}
---
+++ driveschool/src/test/java/com/dolgov/driveschool/DriveschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolgov.driveschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
