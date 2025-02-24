ef3011a6596b9f32d763b29bea42cfa1574df866
frayzkilla <frayzthamotion@gmail.com>
2025-02-24 13:48:36 +0800

    created templates, styles, imgs

---
+++ driveschool/src/main/java/com/dolgov/driveschool/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.dolgov.driveschool.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+
+        return "home";
+    }
+}
